d8de1796cd01e1c5f61daf8d9dba88f39ea06733
Nurul Huda (Apon) <me@nurulhudaapon.com>
2024-07-07 10:29:20 +0600

    fix: remove exec assertion

--- lib/utils.bash
+++ lib/utils.bash
@@ -64,7 +64,7 @@ install_version() {
 		# TODO: Assert sst executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		# test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
